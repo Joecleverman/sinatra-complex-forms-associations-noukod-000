b75e06b1a9d45b388aaa424e8021de2548f763aa
Josue Abraham <josuea@noukod.com>
2019-01-17 17:55:21 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -11,9 +11,14 @@ class OwnersController < ApplicationController
   end
 
   post '/owners' do
-
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    @owner.save
+    redirect to "owners/#{@owner.id}"
   end
-binding.pry
+
   get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
     erb :'/owners/edit'
@@ -24,7 +29,12 @@ binding.pry
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do
-
+  post '/owners/:id' do
+    @owner = Owner.find(params[:id])
+    @owner.update(params["owner"])
+    if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
+    redirect to "owners/#{@owner.id}"
   end
 end
